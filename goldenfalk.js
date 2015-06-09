cd957d3222772bdf63178ece0036be61b0a8e82b
Burak Ozdemir <b.ozdemir@students.rave.ac.uk>
2015-06-09 19:57:12 +0100

    Changes made by burak, collide, when it collides console log you can see it coliding

--- goldenfalk.js
+++ goldenfalk.js
@@ -1,29 +1,43 @@
 var playerSpeed = 120;
 var jumpSpeed = 500;
 var inviahead = 300;
-var game = new Phaser.Game(1300, 500, Phaser.AUTO, '', {
+
+
+var player, 
+	invi, 
+	enemy, 
+	car,
+	wall, 
+	giphy,
+	cursors,
+	platforms;
+
+
+
+var game = new Phaser.Game(1300, 450, Phaser.AUTO, '', {
 	preload: preload,
 	create: create,
 	update: update
 });
 
-var player, invi, bomberman,
-cursors,
-platforms;
 
 function preload() {
-	game.load.image('sky', 'sky.png');
+    game.load.image('giphy','giphy.gif')
 	game.load.spritesheet('pow', 'cowthing.png', 64, 72);
 	game.load.image ('grass', 'grass.png');
 	game.load.image('invi', 'invisible.png',64,72);
-	game.load.spritesheet('bomberman','dude.png',32,48);
+	game.load.image('car', 'Car.png',300,150);
+	game.load.spritesheet('enemy','dude.png',32,48);
+	game.load.image('gameover','game_over.jpg',480,480);
+	game.load.image('wall','brickwall.png',480,480);
+	
 }
 
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
 	game.world.setBounds (0, 0, 8000, game.world.height);
-	game.add.tileSprite(0, 0, game.world.width, game.world.height, 'sky');
+	game.add.tileSprite(0, 0, game.world.width, game.world.height, 'wall');
 
 	player = game.add.sprite(32, game.world.height - 100, 'pow');
 	game.physics.arcade.enable(player);
@@ -37,16 +51,20 @@ function create() {
 	platforms.enableBody = true;
 
 	platform(0, game.world.height - 32, game.world.width, 32);
-	platform (100, 200, 200, 32);
+	platform (300, 400, 300, 150);
+	platform (300, 400, 300, 150);
+
 
 	cursors = game.input.keyboard.createCursorKeys();
 	invi=game.add.image(0, 0, 'invi')
 	game.camera.follow(invi);
 
-	bomberman=game.add.sprite(0, game.world.height - 100, 'bomberman')
-	game.physics.arcade.enable(bomberman);
-	bomberman.body.gravity.y = 600;
-	bomberman.body.collideWorldBounds = true;
+	enemy=game.add.sprite(0, game.world.height - 100, 'enemy')
+	game.physics.arcade.enable(enemy);
+	enemy.body.gravity.y = 600;
+	enemy.body.collideWorldBounds = true;
+
+	
 
 
 }
@@ -55,6 +73,8 @@ function platform(x, y, width, height){
 	var ledge = new Phaser.TileSprite(game, x, y, width, height, 'grass');
 	platforms.add(ledge);
 	ledge.body.immovable = true;
+	
+
 }
 
 
@@ -82,11 +102,75 @@ function update() {
 	//position of invi to be in front of the cow 
 
 	invi.position.x = player.position.x + inviahead;
-	//Puts bomberman on grass
-	game.physics.arcade.collide(bomberman, platforms);
-	//bomberman speed
-	bomberman.body.velocity.x = 100;
+	//Puts enemy on grass
+	game.physics.arcade.collide(enemy, platforms);
+	//enemy speed
+	enemy.body.velocity.x = 100;
 
-	//make bomberman and cow collide
-	game.physics.arcade.collide(player, bomberman);
+	//make enemy and cow collide
+	game.physics.arcade.collide(player, enemy, collisionHandler, null, this);
+
+	//  Run collision
+    //game.physics.arcade.overlap(enemy, player, collisionHandler, null, this);
+    
+    //player.touching.left
+
+
+	 //if (player,enemy=collide () < 1)
+    //{
+        //player.collide();
+       
+//function collisionHandler
+      // if player+enemy collide[display:gameover]
+        //the "click to restart" handler
+     
+    
+	
+	
+	//if(player,enemy=collide){
+		//display:gameover;
+		//else} 
 }
+
+
+
+
+function collisionHandler (enemy, player) {
+
+
+
+
+
+
+
+	console.log('collisionHandler')
+
+    //  When a bullet hits an alien we kill them both
+    /*bullet.kill();
+    alien.kill();
+
+    //  Increase the score
+    score += 20;
+    scoreText.text = scoreString + score;
+
+    //  And create an explosion :)
+    var explosion = explosions.getFirstExists(false);
+    explosion.reset(alien.body.x, alien.body.y);
+    explosion.play('kaboom', 30, false, true);
+
+    if (aliens.countLiving() == 0)
+    {
+        score += 1000;
+        scoreText.text = scoreString + score;
+
+        enemyBullets.callAll('kill',this);
+        stateText.text = " You Won, \n Click to restart";
+        stateText.visible = true;
+
+        //the "click to restart" handler
+        game.input.onTap.addOnce(restart,this);
+    }*/
+}
+
+
+
