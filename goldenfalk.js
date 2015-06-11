5d49916bfe20fd9ea8a8c9e41f79a7b711d22640
Burak Ozdemir <b.ozdemir@students.rave.ac.uk>
2015-06-11 02:58:51 +0100

    Updates finish line, roshida small, platforms update. 3am

--- goldenfalk.js
+++ goldenfalk.js
@@ -1,5 +1,5 @@
 var playerSpeed = 150;
-var jumpSpeed = 500;
+var jumpSpeed = 400;
 var inviahead = 300;
 
 
@@ -16,7 +16,7 @@ var player,
 
 
 
-var game = new Phaser.Game(1000, 450, Phaser.AUTO, '', {
+var game = new Phaser.Game(1300, 450, Phaser.AUTO, '', {
 	preload: preload,
 	create: create,
 	update: update
@@ -29,7 +29,7 @@ function preload() {
 	game.load.image ('grass', 'grass.png');
 	game.load.image('invi', 'invisible.png',64,72);
 	game.load.image('car', 'car.png',256,256);
-	game.load.spritesheet('enemy','smallgolden.png',20,30);
+	game.load.spritesheet('enemy','smallGolden.png',20,30);
 	game.load.image('gameover','game_over.jpg',480,480);
 	game.load.image('wall','brickwall.png',480,480);
 	game.load.image('background','background2.png',700,8000);
@@ -46,37 +46,40 @@ function create() {
 	game.physics.arcade.enable(player);
 	player.body.gravity.y = 600;
 	player.body.collideWorldBounds = true;
-	player.animations.add('left', [0, 1, 2, 3], 10, true);
 	player.animations.add('right', [5, 6, 7, 8], 10, true);
+	player.animations.add('left', [0, 1, 2, 3], 10, true);
+	
 	
 
 	platforms = game.add.group();
 	platforms.enableBody = true;
 
 	platform(0, game.world.height - 32, game.world.width, 32);
-	platform (1500, 350, 90, 20);
-	platform (1700, 320, 90, 20);
-	platform (2200, 330, 90, 30);
-    platform (2390, 310, 90, 30);
-	platform (2500, 270, 110, 30);
-	platform (2700, 250, 150, 30);
-	platform (3100, 330, 90, 20);
-	platform (3400, 330, 90, 30);
-	platform (3500, 320, 90, 30);
-	platform (3800, 330, 60, 20);
-	platform (4200, 330, 60, 30);
-	platform (4600, 330, 60, 30);
-	platform (4700, 290, 60, 30);
-	platform (4900, 330, 60, 20);
-	platform (5100, 330, 60, 20);
-	platform (5150, 310, 80, 20);
-	platform (5400, 330, 60, 30);
-	platform (6000, 330, 60, 30);
-	platform (6300, 330, 60, 30);
-	platform (6800, 330, 60, 30);
-	platform (6900, 330, 80, 30);
-	platform (7100, 320, 30, 50);
-	platform (7300, 170, 80, 190);
+	platform (1500, 355, 90, 20);
+	platform (1700, 355, 90, 20);
+	platform (2200, 355, 90, 30);
+    platform (2390, 340, 90, 30);
+	platform (2500, 320, 110, 30);
+	platform (2700, 300, 150, 30);
+	platform (3100, 355, 90, 20);
+	platform (3400, 355, 90, 30);
+	platform (3500, 355, 90, 30);
+	platform (3800, 355, 60, 20);
+	platform (4200, 355, 60, 30);
+	platform (4600, 355, 60, 30);
+	platform (4700, 355, 60, 30);
+	platform (4900, 355, 60, 20);
+	platform (5100, 355, 60, 20);
+	platform (5150, 355, 80, 20);
+	platform (5400, 355, 60, 30);
+	platform (6000, 355, 60, 30);
+	platform (6300, 355, 60, 30);
+	platform (6800, 355, 60, 30);
+	platform (6900, 355, 80, 20);
+	platform (7050, 300, 80, 20);
+	platform (7170, 250, 100, 20);
+	platform (7300, 1, 80, 90);
+	platform (7300, 150, 80, 280);
 	
 	
 
@@ -123,9 +126,9 @@ function update() {
 	if (cursors.right.isDown) {
 		player.body.velocity.x = playerSpeed;
 		player.animations.play('right');
-	// } else if (cursors.left.isDown) {
-	// 	player.body.velocity.x = -playerSpeed;
-	// 	player.animations.play('left');
+	 } else if (cursors.left.isDown) {
+	 	player.body.velocity.x = -playerSpeed;
+		player.animations.play('left');
 	}
 	else{
 		player.animations.stop();
@@ -175,7 +178,7 @@ function update() {
 
 
 function collisionHandler (enemy, player) {
- game.load.image('gameover', 'game_over.jpg');
+ // game.load.image('gameover', 'game_over.jpg');
 
 
 
